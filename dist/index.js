f26b9a73e1484a78a332f0863007f199b2e3167c
jarora jarora@IN-jarora-W2.adprod.bmc.com <jalajkumar.arora@bmc.com>
2024-09-13 16:25:04 +0530

    update

--- dist/index.js
+++ dist/index.js
@@ -8030,9 +8030,44 @@
               }
               core.info('Setting up the output values');
               const workpace = curWk !== null && curWk !== void 0 ? curWk : '';
+              core.info('Old Code Start')
               //Execution is completed
               try {
+                core.info('Workspace:'+workpace)
                   const autoBuildParms = path.join(workpace, 'automaticBuildParams.txt');
+                  core.info('Autobuildparams:'+autoBuildParms)
+                  if (fs_1.existsSync(autoBuildParms)) {
+                      const dataStr = fs_1.readFileSync(autoBuildParms).toString('utf8');
+                      core.setOutput('automaticBuildJson', dataStr);
+                  }
+              }
+              catch (error) {
+                  if (error instanceof Error) {
+                      core.info(`Fail to read file: automaticBuildParams.txt`);
+                      core.info(error.message);
+                  }
+              }
+              core.info('Old Code End')
+	
+              core.info('New Code Start')
+              //Execution is completed
+              try {
+                core.info('Workspace:'+workpace)
+                const normalizedWorkpace = path.normalize(workpace);
+                core.info('Normalized Workspace'+normalizedWorkpace)
+            
+                // Check if the resolved path is absolute. If not, throw an error
+                if (!path.isAbsolute(normalizedWorkpace)) {
+                  throw new Error('Invalid workspace path: Path must be absolute');
+                }
+            
+                // Ensure that the normalized path doesn't escape to unintended directories (no "../" traversal)
+                if (normalizedWorkpace.includes('..')) {
+                  throw new Error('Potential path traversal detected!');
+                }
+            
+                  const autoBuildParms = path.join(workpace, 'automaticBuildParams.txt');
+                  core.info('Autobuildparams:'+autoBuildParms)
                   if (fs_1.existsSync(autoBuildParms)) {
                       const dataStr = fs_1.readFileSync(autoBuildParms).toString('utf8');
                       core.setOutput('automaticBuildJson', dataStr);
