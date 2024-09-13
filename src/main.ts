7f02232acae933c195cb46591babcf6190e1468c
jarora jarora@IN-jarora-W2.adprod.bmc.com <jalajkumar.arora@bmc.com>
2024-09-13 13:56:35 +0530

    Added Logs

--- src/main.ts
+++ src/main.ts
@@ -30,10 +30,13 @@ async function run(): Promise<void> {
 
     core.info('Setting up the output values')
     const workpace: string = curWk ?? ''
-
+	
+	core.info('Old Code Start')
     //Execution is completed
     try {
+	  core.info('Workspace:'+workpace)
       const autoBuildParms = path.join(workpace, 'automaticBuildParams.txt')
+	  core.info('Autobuildparams:'+autoBuildParms)
       if (existsSync(autoBuildParms)) {
         const dataStr = readFileSync(autoBuildParms).toString('utf8')
         core.setOutput('automaticBuildJson', dataStr)
@@ -44,6 +47,39 @@ async function run(): Promise<void> {
         core.info(error.message)
       }
     }
+	core.info('Old Code End')
+	
+	core.info('New Code Start')
+	try {
+		// Normalize the workspace path to remove any dangerous characters like "../"
+		core.info('Workspace'+Workpace)
+		const normalizedWorkpace = path.normalize(workpace);
+		core.info('Normalized Workspace'+normalizedWorkpace)
+
+		// Check if the resolved path is absolute. If not, throw an error
+		if (!path.isAbsolute(normalizedWorkpace)) {
+			throw new Error('Invalid workspace path: Path must be absolute');
+		}
+
+		// Ensure that the normalized path doesn't escape to unintended directories (no "../" traversal)
+		if (normalizedWorkpace.includes('..')) {
+			throw new Error('Potential path traversal detected!');
+		}
+
+		//const autoBuildParms = path.join(workpace, 'automaticBuildParams.txt')
+		const autoBuildParms = path.join(normalizedWorkpace, 'automaticBuildParams.txt')
+		core.info('Auto Build Parms'+autoBuildParms)
+		if (existsSync(autoBuildParms)) {
+			const dataStr = readFileSync(autoBuildParms).toString('utf8')
+			core.setOutput('automaticBuildJson', dataStr)
+		}
+	} catch (error) {
+		if (error instanceof Error) {
+			core.info(`Fail to read file: automaticBuildParams.txt`)
+			core.info(error.message)
+		}
+	}
+	core.info('New Code End')
 
     try {
       const changedProgs = path.join(workpace, 'changedPrograms.json')
